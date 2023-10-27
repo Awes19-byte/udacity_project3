e451b08b9b6921396d9b0d05c548d5b4266e3def
Oussama Mahfoudhi <o.mahfoudhi@instadeep.com>
2023-10-27 14:30:49 +0100

    add model tests

--- starter/tests/test_project.py
+++ starter/tests/test_project.py
@@ -1,10 +1,36 @@
 """ This module tests the root and the prediction end points """
 from fastapi.testclient import TestClient
-
+import pytest
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import joblib
 # Import our app from main.py.
 from main import app
+from starter.ml.data import process_data
 # Instantiate the testing client with our app.
 client = TestClient(app)
+
+DATA_PATH = 'data/clean_census.csv'
+MODEL_PATH = 'starter/model.pkl'
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+
+@pytest.fixture(name='data')
+def data():
+    """
+    Fixture will be used by the unit tests.
+    """
+    yield pd.read_csv(DATA_PATH)
 
 def test_get_root():
     """ Test the root page get a succesful response"""
@@ -59,3 +85,26 @@ def test_post_predict_down():
 
     assert r.status_code == 200
     assert r.json() == {"Income prediction": "<=50K"}
+
+
+def test_load_data(data):
+    """ Check the data received """
+
+    assert isinstance(data, pd.DataFrame)
+    assert data.shape[0] > 0
+    assert data.shape[1] > 0
+
+
+def test_model():
+    """ Check model type """
+
+    model = joblib.load(MODEL_PATH)
+    assert isinstance(model, RandomForestClassifier)
+
+
+def test_process_data(data):
+    """ Test the data split """
+
+    train, _ = train_test_split(data, test_size=0.20)
+    X, y, _, _ = process_data(train, cat_features, label='salary')
+    assert len(X) == len(y)
